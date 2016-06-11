679a124ee024db86417ecd05f2a81958659376d3
Allen Rohner <arohner@gmail.com>
2016-06-11 18:13:03 -0500

    bump clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha1"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha5"]
                  [org.clojure/tools.analyzer.jvm "0.6.9"]
                  [org.clojure/test.check "0.9.0"]]
   :profiles
