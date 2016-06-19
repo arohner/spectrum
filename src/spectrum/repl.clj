522319e39de5b90312f65e3e4fe3a2b361d5e2ac
Allen Rohner <arohner@gmail.com>
2016-06-19 17:49:20 -0500

    Repl stupids

--- src/spectrum/repl.clj
+++ src/spectrum/repl.clj
@@ -1,6 +1,6 @@
 (ns spectrum.repl
   (:require [clojure.spec :as s]
-            [clojure.tools.analyzer :as ana.jvm]
+            [clojure.tools.analyzer.jvm :as ana.jvm]
             [spectrum.check :as st]
             [spectrum.conform :as c]
             [spectrum.flow :as flow]))
