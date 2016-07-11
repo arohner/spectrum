90e081a33e324882ddd164ee4b44e13f083ddb10
Allen Rohner <arohner@gmail.com>
2016-07-11 16:57:24 -0500

    Bump to clojure 1.9 alpha 10

--- src/spectrum/analyzer_spec.clj
+++ src/spectrum/analyzer_spec.clj
@@ -3,6 +3,6 @@
             [clojure.tools.analyzer.jvm :as ana.jvm]))
 
 (s/def ::ana.jvm/op keyword?)
-(s/def ::ana.jvm/form ::s/any)
+(s/def ::ana.jvm/form any?)
 (s/def ::ana.jvm/analysis (s/keys :req-un [:ana.jvm/op :ana.jvm/form]))
 (s/def ::ana.jvm/analyses (s/coll-of ::ana.jvm/analysis :into []))
