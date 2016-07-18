33f60cd391a165438e9b230684d790995e557e47
Allen Rohner <arohner@gmail.com>
2016-07-18 18:53:05 -0500

    Fix the test arity

--- test/spectrum/examples/bad/map_destructure.clj
+++ test/spectrum/examples/bad/map_destructure.clj
@@ -4,5 +4,5 @@
 (s/def ::message string?)
 (s/fdef foo :args (s/cat :args (s/keys :req-un [::message])) :ret int?)
 
-(defn foo [{:keys [message] :as args} a]
+(defn foo [{:keys [message] :as args}]
   message)
