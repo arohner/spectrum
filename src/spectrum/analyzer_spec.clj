fff0b6d137e72359ed9548abb2a37a43ed720dc2
Allen Rohner <arohner@gmail.com>
2016-07-26 17:47:39 -0500

    Fix specs

--- src/spectrum/analyzer_spec.clj
+++ src/spectrum/analyzer_spec.clj
@@ -4,7 +4,7 @@
 
 (s/def ::ana.jvm/op keyword?)
 (s/def ::ana.jvm/form any?)
-(s/def ::ana.jvm/analysis (s/keys :req-un [:ana.jvm/op :ana.jvm/form]))
+(s/def ::ana.jvm/analysis (s/keys :req-un [::ana.jvm/op ::ana.jvm/form]))
 (s/def ::ana.jvm/analyses (s/coll-of ::ana.jvm/analysis :into []))
 
 (s/def ::variadic boolean?)
