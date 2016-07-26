6e6725531d8dd624d6dacb966d28a113c82f5c1f
Allen Rohner <arohner@gmail.com>
2016-07-26 17:47:44 -0500

    Map destructuring test

---
+++ test/spectrum/examples/good/map_destructure.clj
@@ -0,0 +1,8 @@
+(ns spectrum.examples.good.map-destructure
+  (:require [clojure.spec :as s]))
+
+(s/def ::message string?)
+(s/fdef foo :args (s/cat :x (s/keys :req-un [::message])) :ret string?)
+
+(defn foo [{:keys [message]}]
+  message)
