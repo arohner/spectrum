a1c7fba3417670382199c5b4bfa4f9de4a313cee
Allen Rohner <arohner@gmail.com>
2016-08-12 12:22:06 -0500

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spectrum "0.1.0"
+(defproject spectrum "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
