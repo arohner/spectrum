61ae66dba3fb9e12754f8348478b0c0df8161c3b
Allen Rohner <arohner@gmail.com>
2016-09-05 13:13:43 -0500

    attempts at multispec

--- src/spectrum/analyzer_spec.clj
+++ src/spectrum/analyzer_spec.clj
@@ -10,8 +10,24 @@
 (s/def :ana.jvm.env/column int?)
 (s/def ::ana.jvm/env (s/keys :req-un [:ana.jvm.env/file :ana.jvm.env/line :ana.jvm.env/column]))
 
+(s/def ::ana.jvm/children (s/coll-of keyword? :into []))
+
 (s/def ::ana.jvm/analysis (s/keys :req-un [::ana.jvm/op ::ana.jvm/form]
-                                  :opt-un [::ana.jvm/env]))
+                                 :opt-un [::ana.jvm/env
+                                          ::ana.jvm/children]))
+
+(defmulti analysis-type :op)
+
+(defmethod analysis-type :default [_]
+  ::analysis-common)
+
+(s/def ::ana.jvm/fn ::ana.jvm/analysis)
+(s/def ::ana.jvm/args (s/coll-of ::ana.jvm/analysis :into []))
+
+(defmethod analysis-type :invoke [_]
+  (s/merge ::analysis-common (s/keys :req-un [::ana.jvm/fn ::ana.jvm/args])))
+
+;;(s/def ::ana.jvm/analysis (s/multi-spec analysis-type :op))
 
 (s/def ::ana.jvm/analyses (s/* ::ana.jvm/analysis))
 
