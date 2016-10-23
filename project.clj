a8b2738ecbcfe28630f237b3b1ddb65aaf3254d9
Allen Rohner <arohner@gmail.com>
2016-10-23 15:31:29 -0500

    Bump clojure

--- project.clj
+++ project.clj
@@ -3,11 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha12"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
                  [org.clojure/tools.analyzer.jvm "0.6.10"]
                  [org.clojure/test.check "0.9.0"]]
   :profiles
-  {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
+  {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
+                        [arohner/repl-utils "0.1.0"]]
          :repl-options {:init-ns spectrum.repl}}}
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :creds :gpg}]]
