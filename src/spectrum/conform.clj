000c7354449619a7e26a2d47475e57f4fd6524ca
Allen Rohner <arohner@gmail.com>
2016-11-16 16:43:50 +0900

    relax spect-like

--- src/spectrum/conform.clj
+++ src/spectrum/conform.clj
@@ -108,8 +108,12 @@
 (defn fn-invoke? [x]
   (and (seq? x) (symbol? (first x))))
 
+
+;; spect-like is hard. It should be 'anything that can be parsed into a spect (spec, symbol, var, keyword)' + 'anything that can be parsed into a value (any?)'. That's pretty wide, so this is just any?. There may be a narrower type, but I'm not sure what it is.
+
 ;; a thing that parse-spec will return a valid ::spect on
-(s/def ::spect-like (s/or :spec s/spec? :spect ::spect :key keyword? :sym symbol? :var var? :fn fn-invoke? :nil nil?))
+(s/def ::spect-like any? ;;(s/or :spec s/spec? :spect ::spect :key keyword? :sym symbol? :var var? :fn fn-invoke? :nil nil?)
+  )
 
 (s/fdef conform* :args (s/cat :spec spect? :x any?))
 
