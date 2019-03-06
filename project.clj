1ac459fb9b53d4f90c08e679df3d510114144ff4
Allen Rohner <arohner@gmail.com>
2019-03-06 13:09:09 -0600

    Version 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spectrum "0.2.1-SNAPSHOT"
+(defproject spectrum "0.2.1"
   :description "Static 'type' checking for clojure.spec"
   :url "http://github.com/arohner/spectrum"
   :license {:name "Attribution-NonCommercial-NoDerivs 3.0 Unported"
